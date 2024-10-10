2dd45d24fc8ee84bc8a009fcd71d155f17e951eb
KafkaDu <banshee1115@163.com>
2024-10-10 10:05:24 +0800

    增加登录loading的spin

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,7 +2,7 @@
  * @Author: KafkaDu banshee1115@163.com
  * @Date: 2024-09-11 11:04:10
  * @LastEditors: KafkaDu banshee1115@163.com
- * @LastEditTime: 2024-10-09 17:14:28
+ * @LastEditTime: 2024-10-10 10:05:02
  * @FilePath: \file-storage\src\views\DesktopView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
