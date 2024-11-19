dbd75024b3563c91af2132098d29dbcbe17898af
AkanaDu <banshee1115@163.com>
2024-11-19 14:51:47 +0800

    用户退出菜单功能完成

--- src/components/Layouts/User.tsx
+++ src/components/Layouts/User.tsx
@@ -2,7 +2,7 @@
  * @Author: AkanaDu banshee1115@163.com
  * @Date: 2024-11-18 17:08:06
  * @LastEditors: AkanaDu banshee1115@163.com
- * @LastEditTime: 2024-11-19 14:44:56
+ * @LastEditTime: 2024-11-19 14:50:53
  * @FilePath: /file-storage/src/components/Layouts/User.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,7 @@ import { defineComponent } from 'vue'
 import type { PropType } from 'vue'
 import './User.scss'
 import { Avatar, Dropdown, Menu, MenuItem } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
 
 interface User {
   name: string
@@ -28,16 +29,28 @@ export default defineComponent({
     const overlayContent = () => (
       <Menu>
         <MenuItem>
-          <a href="javascript:;">个人中心</a>
+          <span class="hover:text-blue-500 transition-all">个人中心</span>
         </MenuItem>
         <MenuItem>
-          <a href="javascript:;">设置中心</a>
+          <span class="hover:text-blue-500 transition-all">设置中心</span>
         </MenuItem>
         <MenuItem>
-          <a href="javascript:;">退出系统</a>
+          <span
+            class="hover:text-blue-500 transition-all"
+            onClick={(event) => {
+              exitSystem(event)
+            }}
+          >
+            退出系统
+          </span>
         </MenuItem>
       </Menu>
     )
+
+    const router = useRouter()
+    const exitSystem = () => {
+      router.push('/login')
+    }
     return () => (
       <Dropdown v-slots={{ overlay: overlayContent }}>
         <Avatar
